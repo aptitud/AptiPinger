b802635f54c5b3eec0f2e2a1f89bc34cc66e1e25
Hugo <hugo.haggmark@gmail.com>
2015-12-18 12:52:25 +0100

    Fixed bug

--- server.js
+++ server.js
@@ -41,8 +41,7 @@ var server = http.createServer(function(request, response) {
   response.writeHead(200, {
     "Content-Type": "text/html"
   });
-  response.write("<!DOCTYPE "
-    html ">");
+  response.write("<!DOCTYPE html >");
   response.write("<html>");
   response.write("<head>");
   response.write("<title>Hello World Page</title>");

13122ceafbd10d6c7e2ba75d2341aacd0fa2cbae
Christian Fredh <christian.fredh@gmail.com>
2015-12-18 13:19:51 +0100

    Added pinging for aptitud.se

--- server.js
+++ server.js
@@ -5,28 +5,37 @@ var email = require('emailjs');
 var http = require('http');
 var appPort = Number(process.env.PORT || 2013);
 
-const PING_URL = "http://aptigram.apphb.com/";
+const pingUrls = [
+    "http://aptigram.apphb.com/",
+    "http://aptitud.se/"
+];
+
 const TIME_FORMAT_PATTERN = "YYYY-MM-DD, HH:mm:ss:SSS Z";
 
-/**
- * Cron job
- */
-try {
-  new cronJob('* * * * *', function() {
-    var jobExecutionTime = moment();
-    request(PING_URL, function(error, response, scrapedForecasts) {
-      if (!error) {
-        log("Success pinging");
-      } else {
-        var errorMessage = "Error pinging: " + error;
+for (var urlIndex = 0; urlIndex < pingUrls.length; urlIndex++) {
+    var url = pingUrls[urlIndex];
+    setupPingerFor(url);
+}
+
+function setupPingerFor(url)
+{
+    try {
+        new cronJob('* * * * *', function () {
+            var jobExecutionTime = moment();
+            request(url, function (error, response, scrapedForecasts) {
+                if (!error) {
+                    log("Success pinging URL: " + url);
+                } else {
+                    var errorMessage = "Error pinging URL: " + url + " Error: " + error;
+                    log(errorMessage);
+                }
+            });
+        }, null, true);
+        log("Cron job started for URL: " + url);
+    } catch (ex) {
+        var errorMessage = "Cron pattern not valid: " + ex;
         log(errorMessage);
-      }
-    });
-  }, null, true);
-  log("Cron job started");
-} catch (ex) {
-  var errorMessage = "Cron pattern not valid: " + ex;
-  log(errorMessage);
+    }
 }
 
 function log(logMessage) {
